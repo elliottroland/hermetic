7c4da74a0b5782563b6fe5b6447bf4919b3df907
Roland Elliott <rolae@amazon.com>
2026-03-13 13:05:14 +0200

    Pull out separate Defer and Either scope interfaces, add DslMarker to prevent nesting of defer {}

--- src/hermetic/either/Defer.kt
+++ src/hermetic/either/Defer.kt
@@ -2,7 +2,99 @@ package hermetic.either
 
 import hermetic.effects.*
 import hermetic.effects.fs.*
+import java.util.concurrent.ConcurrentLinkedDeque
 import kotlin.io.writer
+
+/**
+ * Executes the [block] within a [DeferScope]. Generally, it is recommended that you use the [either] for handling
+ * deferring code, but if you don't need the extra machinery around error handling then this is a much simpler
+ * alternative. The value passed to the each [defer][DeferScope] call will be the computed return value if the [block]
+ * exitted normally, or null if it ended exceptionally. If the [block] or any of the [defer][DeferScope] calls end
+ * exceptionally, the first observed exception is thrown.
+ * 
+ * @see DeferScope.defer
+ */
+inline fun <R> defers(block: DeferScope<R?, EmptyDeferBlockScope>.() -> R): R {
+    val scope = DeferScope<R?, EmptyDeferBlockScope>()
+    var result: R? = null
+    try {
+        result = scope.block()
+        return result
+    } finally {
+        if (scope.hasDeferrals()) {
+            var deferError: Exception? = null
+            for (deferral in scope.deferrals) {
+                try {
+                    EmptyDeferBlockScope.deferral(result)
+                } catch (e: Exception) {
+                    if (deferError == null && result == null) {
+                        deferError = e
+                    }
+                }
+            }
+            deferError?.also { throw it }
+        }
+    }
+}
+
+/**
+ * Defines a scope which exposes various functions for deferring computations until the end of the scope, whether it
+ * end with a success or failure.
+ */
+@DeferMarker
+open class DeferScope<R, BlockScope : DeferBlockScope> {
+    private val lazyDeferrals = lazy(LazyThreadSafetyMode.PUBLICATION) {
+        ConcurrentLinkedDeque<BlockScope.(R) -> Unit>()
+    }
+    @PublishedApi internal val deferrals by lazyDeferrals
+
+    @PublishedApi internal fun hasDeferrals(): Boolean = lazyDeferrals.isInitialized()
+
+    /**
+     * Defer execution of the [block] until the end of the [either] scope, regardless of whether the
+     * latter ended successfully. Defer blocks are executed in reverse order from their definitions, to
+     * allow for the proper cleanup of resources.
+     */
+    fun defer(block: BlockScope.(R) -> Unit) {
+        deferrals.addFirst(block)
+    }
+
+    /**
+     * Defers calling [close]. If the call throws an exception, then [onException] will be called in
+     * order to provide an opportunity for mapping the exception to a modelled error. If the
+     * [onException] block is not provided, then the exception is thrown as normal. As with normal defer
+     * calls, you may use the [EitherScope]'s failure methods for modeling failures and it is passed the
+     * current response of the block.
+     *
+     * This is defined on the closeable so that you can easily defer its closure as soon as possible
+     * after creation:
+     * ```
+     * val writer = file.writer().getOrFail().deferClose()
+     * ```
+     *
+     * This function be thought of as a generalization of [kotlin.io.use], in that it supports the
+     * automatic closure of [AutoCloseable]s, but in a way which is compatible with [Either] semantics.
+     */
+    fun <T : AutoCloseable> T.deferClose(onException: (R, Exception) -> Unit = { _, ex -> throw ex }): T =
+        apply {
+            defer { response ->
+                try {
+                    close()
+                } catch (e: Exception) {
+                    onException(response, e)
+                }
+            }
+        }
+}
+
+@DeferMarker
+interface DeferBlockScope
+
+object EmptyDeferBlockScope : DeferBlockScope
+
+@DslMarker
+@Target(AnnotationTarget.CLASS, AnnotationTarget.TYPE)
+annotation class DeferMarker
 
 fun main() {
     val fs = GlobalFileSystem(null)
@@ -21,7 +113,9 @@ fun testDefer(): Either<String, Long> = either {
     defer {
         log.info("Deleting dir: $dir")
         fs.delete(dir).getOrFail { "Failed to clean up dir: $it" }
-        fs.getDir(dir.path).getOrFail { "Failed to clean up dir: $it" }
+        // defer {
+        //     println("Nested defer is executing")
+        // }
     }
 
     val file = fs.createFile(dir.resolve("some-file")).getOrFail { "Failed to create file: $it" }
@@ -31,8 +125,8 @@ fun testDefer(): Either<String, Long> = either {
         fs.delete(file).getOrFail { "Failed to clean up file: $it" }
     }
 
-    val writer = file.writer().getOrFail { "Could not create writer: $it" }.deferClose()
-    writer.deferClose().write("Hello, world!")
+    val writer = file.writer().getOrFail { "Could not create writer: $it" }
+    writer.use { it.write("Hello, world!") }
 
     log.info("File contents: ${file.reader().getOrThrow().readLines()}")
     file.sizeBytes
---
+++ src/hermetic/either/EitherScope.kt
@@ -0,0 +1,152 @@
+package hermetic.either
+
+/**
+ * Executes the [block] with an [EitherScope], wrapping the successful result with [ok] and wrapping
+ * any failures with [err]. Within the scope, failure is managed via the extra functions provided,
+ * such as [getOrFail][EitherScope.getOrFail] and [fail][EitherScope.fail], which short-circuit the
+ * block for the [err] branch.
+ *
+ * ### Defer
+ * Within the either scope you may [defer][EitherScope.defer] a computation until the
+ * block is finished, regardless of whether it finishes with a success of failure. Defer blocks are
+ * run in reverse order from their definitions.
+ *
+ * The response which will be returned by the [either] is passed to each deferral, allowing them to
+ * change their behavior based on it (although this is not recommended in general, as it complicates
+ * the logic). Each deferral runs within the same [scope][EitherScope] as the rest of the block,
+ * meaning that it can decide to ultimately fail using the [getOrFail][EitherScope.getOrFail] and
+ * [fail][EitherScope.fail] functions.
+ *
+ * A deferral's failure will override a successful response from the either. If the either block
+ * itself failed, then this error is kept. If two deferrals fail, then the earliest failure in time
+ * (reverse of definition) is the one which is chosen.
+ * ```
+ * either {
+ *   val dir = fs.createDir("my-dir").getOrFail()
+ *   defer { fs.delete(dir).getOrFail() }
+ *
+ *   val file = fs.createFile(dir, "my-file")
+ *   defer { fs.delete(file).getOrFail() }
+ *
+ *   // Do work with file
+ * } // File will be deleted, then the directory
+ * ```
+ *
+ * ### Multiple nested scopes
+ * While it is not recommended (because it is difficult to read), you can nest [either] scopes and
+ * break to outer ones using [with]:
+ * ```
+ * either outer@{
+ *   // runs
+ *   either inner@{
+ *     // runs
+ *     with(this@outer) { fail(X) }
+ *     // does not run
+ *   }
+ *   // does not run
+ * } // resolves to err(X)
+ * ```
+ */
+inline fun <E, O> either(block: EitherDeferScope<E, O>.() -> O): Either<E, O> {
+    val scope = EitherDeferScope<E, O>()
+    var result: Either<E, O>? = null
+    try {
+        result = ok(scope.block())
+    } catch (e: ErrPropagation) {
+        when {
+            e.scope === scope -> result = Either(e.value)
+            else -> throw e
+        }
+    } finally {
+        if (scope.hasDeferrals()) {
+            for (deferral in scope.deferrals) {
+                try {
+                    scope.deferral(result!!)
+                } catch (e: ErrPropagation) {
+                    when {
+                        e.scope === scope -> {
+                            if (result?.isErr != true) {
+                                result = Either(e.value)
+                            }
+                        }
+                        else -> throw e
+                    }
+                }
+            }
+        }
+    }
+    return result
+}
+
+
+/** See [EitherScope] for details on how this is used. */
+@PublishedApi internal class ErrPropagation(val value: Err<*>, val scope: EitherScope<*, *>) : Throwable(null, null, false, false)
+
+interface EitherScope<E, O> : DeferBlockScope {
+    /**
+     * Returns the value in this [Either] if it is on the [ok] branch, otherwise fails the current
+     * [either] scope with the value in the [err] branch:
+     * ```
+     * fun myFunction(): Either<Failure, String> = either {
+     *   val result = possiblyFailingCall().getOrFail()
+     *   // does not run if previous call failed
+     * }
+     * ```
+     */
+    fun <O> Either<E, O>.getOrFail(): O
+
+    /**
+     * Returns the value in this [Either] if it is on the [ok] branch, otherwise fails the current
+     * [either] scope with the result of the [mapper]. This allows you to transform failures into the
+     * expected type before short-circuiting the current [either] scope.
+     * ```
+     * fun possiblyFailingCall(): Either<Failure, String> {
+     *   // Some possibly failing code
+     * }
+     *
+     * fun mapFailure(failure: Failure): OtherFailure {
+     *   // Maps between received and expected failures
+     * }
+     *
+     * fun myFunction(): Either<OtherFailure, String> = either {
+     *   val result = possiblyFailingCall().getOrFail { err -> mapFailure(err) }
+     *   // does not run if previous call failed
+     * }
+     * ```
+     */
+    fun <F, O> Either<F, O>.getOrFail(mapper: (F) -> E): O
+
+    /**
+     * Short-circuits the current [either] scope, resolving it's overall value to the [err] branch with
+     * value [e]:
+     * ```
+     * either {
+     *   // runs
+     *   fail(X)
+     *   // does not run
+     * } // resolves to err(X)
+     * ```
+     */
+    fun fail(e: E): Nothing
+}
+
+/**
+ * The scope in which [either]s are executed, which provides more functionality for controlling the
+ * flow of the function in ways that are not supported in general. Within an [either] scope, the
+ * return value is wrapped in an [ok], while any failures short-circuit the block and determine the
+ * value in an [err].
+ *
+ * @see either
+ */
+class EitherDeferScope<E, O> : EitherScope<E, O>, DeferScope<Either<E, O>, EitherScope<E, O>>() {
+    // ------ Failures ------
+
+    override fun <O> Either<E, O>.getOrFail(): O =
+        getOr { fail(it) }
+
+    override fun <F, O> Either<F, O>.getOrFail(mapper: (F) -> E): O =
+        getOr { fail(mapper(it)) }
+
+    override fun fail(e: E): Nothing =
+        throw ErrPropagation(Err(e), this@EitherDeferScope)
+}
